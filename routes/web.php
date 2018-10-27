58408637ab827889e2f89242407e56c72718dde5
imanalopher <iman.mamadiyev@gmail.com>
2018-10-27 12:02:29 +0600

    home students list

--- routes/web.php
+++ routes/web.php
@@ -35,7 +35,7 @@ Route::prefix('teacher')->group(function() {
 Route::prefix('admin')->group(function() {
     Route::get('/login', 'Auth\AdminLoginController@showLoginForm')->name('admin.login');
     Route::post('/login', 'Auth\AdminLoginController@login')->name('admin.login.submit');
-    Route::get('/home', 'StudentController@index')->name('admin.home');
+    Route::get('/home', 'AdminController@index')->name('admin.home');
     Route::get('/', 'AdminController@index')->name('admin.home');
     Route::get('/logout', 'AdminController@logout')->name('admin.logout');
     Route::group(['as' => 'admin.'], function() {
