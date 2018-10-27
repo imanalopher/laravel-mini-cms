b368c9c292fc9b62bbecc77b80ebaaac5617b3da
imanalopher <iman.mamadiyev@gmail.com>
2018-10-27 18:44:53 +0600

    Schools create page

--- resources/views/school/create.blade.php
+++ resources/views/school/create.blade.php
@@ -2,21 +2,30 @@
 
 @section('content')
     <div class="container">
-        <div class="offset-3 col-6">
-            <form action="{{ route('admin.school.store') }}" method="post">
-                @csrf
-                <div class="form-group row">
-                    <label for="title" class="col-sm-2 col-form-label">Название</label>
-                    <div class="col-sm-10">
-                        <input type="text" class="form-control" id="name" name="name" placeholder="Название">
-                    </div>
+        <div class="row">
+            <div class="col-lg-12 float-right">
+                <div class="pull-left">
+                    <h2>Создать новый Школ</h2>
                 </div>
-                <div class="form-group row">
-                    <div class="col-sm-10">
-                        <button type="submit" class="btn btn-primary">Создать</button>
+            </div>
+        </div>
+        <div class="row">
+            <div class="offset-3 col-6">
+                <form action="{{ route('admin.school.store') }}" method="post">
+                    @csrf
+                    <div class="form-group row">
+                        <label for="title" class="col-sm-2 col-form-label">Название</label>
+                        <div class="col-sm-10">
+                            <input type="text" class="form-control" id="name" name="name" placeholder="Название">
+                        </div>
                     </div>
-                </div>
-            </form>
+                    <div class="form-group row">
+                        <div class="col-sm-10">
+                            <button type="submit" class="btn btn-primary">Создать</button>
+                        </div>
+                    </div>
+                </form>
+            </div>
         </div>
     </div>
 @endsection
--- resources/views/school/index.blade.php
+++ resources/views/school/index.blade.php
@@ -2,35 +2,50 @@
 
 @section('content')
     <div class="container">
-        {{ $schools->links() }}
-        <table class="table table-striped">
-            <thead>
-            <tr>
-                <th>ID</th>
-                <th>Название</th>
-                <th></th>
-            </tr>
-            </thead>
-            <tbody>
-                @foreach($schools as $school)
+        <div class="row">
+            <div class="col-lg-12 float-right">
+                <div class="pull-left">
+                    <h2>Список Школ</h2>
+                </div>
+                <div class="float-right">
+                    <a class="btn btn-success" href="{{ route('admin.school.create') }}"> Create New Product</a>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-12">
+                {{ $schools->links() }}
+                <table class="table table-striped">
+                    <thead>
                     <tr>
-                        <td>{{ $school->id }}</td>
-                        <td>{{ $school->name }}</td>
-                        <td><a class="btn btn-info" href="{{ route('admin.school.edit', $school) }}">Редактировать</a></td>
-                        <td>
-                            <form action="{{ route('admin.school.destroy', $school->id) }}" method="POST">
-
-                                @csrf
-                                @method('DELETE')
-
-
-                                <button type="submit" class="btn btn-danger">Delete</button>
-                            </form>
-                        </td>
+                        <th>ID</th>
+                        <th>Название</th>
+                        <th></th>
+                        <th></th>
                     </tr>
-                @endforeach
-            </tbody>
-        </table>
-        {{ $schools->links() }}
+                    </thead>
+                    <tbody>
+                    @foreach($schools as $school)
+                        <tr>
+                            <td>{{ $school->id }}</td>
+                            <td>{{ $school->name }}</td>
+                            <td><a class="btn btn-info" href="{{ route('admin.school.edit', $school) }}">Редактировать</a></td>
+                            <td>
+                                <form action="{{ route('admin.school.destroy', $school->id) }}" method="POST">
+
+                                    @csrf
+                                    @method('DELETE')
+
+
+                                    <button type="submit" class="btn btn-danger">Delete</button>
+                                </form>
+                            </td>
+                        </tr>
+                    @endforeach
+                    </tbody>
+                </table>
+                {{ $schools->links() }}
+            </div>
+        </div>
     </div>
 @endsection
